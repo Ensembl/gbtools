1e81603a7e8635e19341dcfce7f00e38283b61e2
Gemma Barson <gb10@sanger.ac.uk>
2015-04-23 09:35:09 +0100

    Update authors - DOC

--- src/include/gbtools/gbtoolsGUI.hpp
+++ src/include/gbtools/gbtoolsGUI.hpp
@@ -19,15 +19,7 @@
  * ---------------------------------------------------------------------------
  * This file is part of the gbtools genome browser tools library, 
  * written by
- *      Gemma Barson      (Sanger Institute, UK)  <gb10@sanger.ac.uk>
- * 
- * and utilizing code taken from the AceDB, ZMap and SeqTools packages, written by
- *      Richard Durbin    (Sanger Institute, UK)  <rd@sanger.ac.uk>
- *      Erik Sonnhammer   (SBC, Sweden)           <Erik.Sonnhammer@sbc.su.se>
- *      Jean Thierry-Mieg (CRBM du CNRS, France)  <mieg@kaa.crbm.cnrs-mop.fr>
  *      Ed Griffiths      (Sanger Institute, UK)  <edgrif@sanger.ac.uk>
- *      Roy Storey        (Sanger Institute, UK)  <rds@sanger.ac.uk>
- *      Malcolm Hinsley   (Sanger Institute, UK)  <mh17@sanger.ac.uk>
  *      Gemma Barson      (Sanger Institute, UK)  <gb10@sanger.ac.uk>
  *      Steve Miller      (Sanger Institute, UK)  <sm23@sanger.ac.uk>
  *
