e8fddb79af05e07b6233c8fc572e1e5e2c727f5c
sgiorgetti <sgiorgetti@ebi.ac.uk>
2021-03-19 14:50:22 +0000

    test

--- src/gbtoolsPfetch/gbtoolsPfetchHttp.cpp
+++ src/gbtoolsPfetch/gbtoolsPfetchHttp.cpp
@@ -157,6 +157,10 @@ namespace gbtools
       
         pfetch_data->request_counter++ ;
 
+        // stefano
+        g_warning("ste-DEBUG: About to call CURLObjectPerform\n") ;
+        g_warning("ste-DEBUG: CURLObjectPerform-location: >%s<\n", pfetch_data->curl_object->url) ;
+
         if (CURLObjectPerform(pfetch_data->curl_object, TRUE) == CURL_STATUS_FAILED)
           {
             char *curl_object_error = NULL;
