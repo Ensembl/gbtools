858600c2c06c8165328857ac5df288ed3f5c3842
mira <mira@ebi.ac.uk>
2021-06-23 03:30:31 +0100

    Update license

--- src/blatSrc/lib/aliType.c
+++ src/blatSrc/lib/aliType.c
@@ -1,12 +1,12 @@
 /*
- * Copyright [2018] EMBL-European Bioinformatics Institute
- * 
+ * Copyright [2018-2021] EMBL-European Bioinformatics Institute
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,7 @@
 
 /* aliType - some definitions for type of alignment. */
 
-/* Copyright (C) 2011 The Regents of the University of California 
+/* Copyright (C) 2011 The Regents of the University of California
  * See README in this or parent directory for licensing information. */
 #include "common.h"
 #include "aliType.h"
@@ -46,4 +46,3 @@ if (sameWord(name, "RNAX")) return gftRnaX;
 errAbort("Unknown sequence type '%s'", name);
 return 0;
 }
-
